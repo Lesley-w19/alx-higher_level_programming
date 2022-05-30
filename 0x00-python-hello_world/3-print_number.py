6a3dd2521162bd1cccfd9acd4195799e1d96ce8c
Lesley-w19 <lesley.kamamo@gmail.com>
2022-05-30 13:07:41 -0700

    3. Print integer

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+number = 98
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("{:d} Battery Street".format(number));
--- 0x00-python-hello_world/README.md
+++ 0x00-python-hello_world/README.md
@@ -2,3 +2,4 @@
 0. Run Python file
 1.Run inline
 2. Hello, print
+3. Print integer
