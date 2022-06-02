c6fd923cd85a179c332f3b2b6f41410c896bc496
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-02 21:22:06 +0000

    8. Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-stra = str[:6];
-strb = str[39:66]
-strc = str[106:111]
-str = strb + strc + " " + stra
+str = str[39:66] + str[106:111] + " " + str[:6]
 print(str)
