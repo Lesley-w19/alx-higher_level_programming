7f8a04ba1a04fff5a6e5d14da82338be70576c9c
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-02 22:32:44 +0000

    2. How to make a script dynamic!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    size = len(sys.argv)
+    n = len(sys.argv) - 1
+    if n == 0:
+        print(".")
+    elif n == 1:
+        print("{:d} argument.".format(n))
+    else:
+        print("{:d} arguments.".format(n))
+    
+    for n in range(1, size):
+        print("{:d}: {}".format(n, sys.argv[n]))
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -1,3 +1,4 @@
 0x02-python-import_modules
 0. Import a simple function from a simple file
 1. My first toolbox!
+2. How to make a script dynamic!
