f8936fa2c0896f463d6012191221b238a594e3c8
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-02 22:55:40 +0000

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    rslt = 0
+    for n in sys.argv:
+        if n != sys.argv[0]:
+            rslt += int(n)
+        print(rslt)
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -2,3 +2,4 @@
 0. Import a simple function from a simple file
 1. My first toolbox!
 2. How to make a script dynamic!
+3. Infinite addition
