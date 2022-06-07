2f05ad511981b010599425f6201996bc9f2f5044
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-07 10:10:07 +0000

    8. More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    tuple_end = ()
+    if len(sentence) == 0:
+        tuple_end = len(sentence), None
+        return tuple_end
+    else:
+        tuple_end = len(sentence), sentence[0]
+        return tuple_end
+    
--- 0x03-python-data_structures/README.md
+++ 0x03-python-data_structures/README.md
@@ -7,3 +7,4 @@
 5. Can you C me now?
 6. Lists of lists = Matrix
 7. Tuples addition
+8. More returns!
