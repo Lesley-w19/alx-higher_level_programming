10d0c1747aadb96eb501be0ed486ca677ea379d7
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-10 19:56:48 +0000

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a_lower in range(97, 123):
-    if a_lower != 101 and a_lower != 113:
+    if (a_lower != 101 and a_lower != 113):
         print("{:c}".format(a_lower), end='')
