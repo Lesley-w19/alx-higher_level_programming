47d6a20489346a278039ac702a93d2170099a02a
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-11 00:34:30 +0000

    9. Multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     n_dir = a_dictionary.copy()
-    l_kesys = list(n_dir.keys())
+    l_keys = list(n_dir.keys())
 
     for d in l_keys:
         n_dir[d] *= 2
