62aeb6021099217db8713953ee9a041f69bdfb16
Lesley-w19 <lesley.kamamo@gmail.com>
2022-06-27 05:33:52 -0700

    3. String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+class Rectangle:
+    """class Rectange with private and public instances"""
+
+    _Rectangle__height = None
+    _Rectangle_width = None
+
+    def __init__(self, widthVal=0, heightVal=0):
+        self.height = heightVal
+        self.width = widthVal
+
+    @property
+    def height(self):
+        return self._Rectangle__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be  >= 0")
+        self._Rectangle__height = value
+
+    @property
+    def width(self):
+        return self._Rectangle__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be  >= 0")
+        self._Rectangle__width = value
+
+    def area(self):
+        return (self.height * self.width)
+
+    def perimeter(self):
+        if (self.height == 0 or self.width == 0):
+            return 0
+        else:
+            return (self.height + self.width) * 2
+    
+    def __str__(self):
+        rslt = ""
+
+        if self.height ==0 or self.width == 0:
+            rslt += ''
+        else:
+            for row in range(self.height):
+                rslt += "#" * self.width
+                if (row < self.height - 1):
+                    rslt += '\n'
+
+        return rslt
