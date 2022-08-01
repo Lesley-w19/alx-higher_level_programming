701b1313b917c3e184eeac810887312e8890d8d7
Lesley-w19 <lesley.kamamo@gmail.com>
2022-08-01 02:35:00 -0700

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,10 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(value), end='')
             num_of_list += 1
+        
         except ValueError:
             pass
-        print()
+
+        print(value)
 
         return num_of_list
