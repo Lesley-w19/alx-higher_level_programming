e7a70a063fef3ff2e2345116515e7ed96564e9e2
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-16 09:03:28 +0300

    script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0
 
-SELECT * FROM first_table WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
