e09ef27eb807f4ad8009606aa2656cffa7b1d54a
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-16 23:46:11 +0300

    -- a script that creates the table unique_id on your MySQL server.
    -- unique_id description:
    -- id INT with the default value 1 and must be unique
    -- name VARCHAR(256)
    -- The database name will be passed as an argument of the mysql command

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -4,5 +4,6 @@
 -- id INT with the default value 1 and must be unique
 -- name VARCHAR(256)
 -- The database name will be passed as an argument of the mysql command
+
 CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1, name VARCHAR(256) NOT NULL);
 ALTER TABLE unique_id ADD CONSTRAINT unique_id_constraint UNIQUE KEY(id);
