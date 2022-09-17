ee1c3187253c11cb90719c0f835dc1e93e2130ad
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-17 15:59:04 +0300

    19. Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,8 @@
 -- a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-USE hbtn_0d_tvshows_rate;
-SELECT shws.title, SUM(shw_rting.rate) AS rating FROM tv_shows shws LEFT JOIN tv_show_ratings shw_rting ON shw_rting.show_id
-= shws.id GROUP BY shws.title ORDER BY rating DESC;
+-- USE hbtn_0d_tvshows_rate;
+SELECT shws.title, 
+SUM(shw_rting.rate) AS rating 
+FROM tv_shows shws 
+LEFT JOIN tv_show_ratings shw_rting ON shw_rting.show_id = shws.id 
+GROUP BY shws.title 
+ORDER BY rating DESC;
