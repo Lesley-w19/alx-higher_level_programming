81a6108fee55a6f810e9cd2da1da592b4988d8a2
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-17 13:10:12 +0300

    10. Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,5 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
-USE hbtn_0d_tvshows;
+-- USE hbtn_0d_tvshows;
 SELECT shws.title, shw_g.genre_id FROM tv_shows shws INNER JOIN tv_show_genres shw_g ON shw_g.show_id = shws.id ORDER BY shws.title ASC, shw_g.genre_id ASC;
