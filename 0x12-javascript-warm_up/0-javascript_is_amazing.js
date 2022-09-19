8e9b4caf8fde88b0e48646af85004d9e9f150095
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-19 16:45:02 +0300

    removed commas

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+
 /**
  *  Write a script that prints “JavaScript is amazing”
  */
 
-const myVar = "JavaScript is amazing";
-console.log(myVar);
+const myVar = "JavaScript is amazing"
+console.log(myVar)
