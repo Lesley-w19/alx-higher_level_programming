7498e856bef97244782b48bc28e7a71def6b2580
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-19 16:55:02 +0300

    changed to single quotes

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -7,6 +7,6 @@
  * The third line: “JavaScript is amazing”
  */
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
