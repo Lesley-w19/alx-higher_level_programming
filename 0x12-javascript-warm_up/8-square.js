7ac1db4c6e9a44ea47ad7d8da3471efc1b173ad2
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-19 22:15:41 +0300

    process.argv[2]

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,13 +5,13 @@
  */
 
 if (isNaN(parseInt(process.argv[2]))) {
-  console.log('Missing argument');
+  console.log("Missing argument");
 } else {
-  const x = parseInt(process.argv[2]);
-  for (let rw = 0; rw < x; rw++) {
-    for (let cl = 0; cl < x; cl++) {
-      process.stdout.write('X');
+  let cnt = process.argv[2];
+  for (let rw = 0; rw < cnt; rw++) {
+    for (let cl = 0; cl < cnt; cl++) {
+      process.stdout.write("X");
     }
-    process.stdout.write('\n');
+    process.stdout.write("\n");
   }
 }
