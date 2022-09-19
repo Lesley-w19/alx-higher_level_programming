ea51f79000f5e5208a2562095c507c8d70fff106
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-19 22:19:25 +0300

    a script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing argument');
 } else {
-  let x = parseInt(process.argv[2]);
+  const x = parseInt(process.argv[2]);
   for (let rw = 0; rw < x; rw++) {
     for (let cl = 0; cl < x; cl++) {
       process.stdout.write('X');
