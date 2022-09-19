51d4853235ee86bf33fb5208b1333404701f5c3c
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-19 18:06:36 +0300

    3. Value of my argument
    Write a script that prints the first argument passed to it:

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * a script that prints the first argument passed to it: argv is 2
+ */
+
+if (process.argv[2] === undefined ){
+    console.log('No argument');
+} else {
+    console.log(process.argv[2]);
+}
--- 0x12-javascript-warm_up/README.md
+++ 0x12-javascript-warm_up/README.md
@@ -5,3 +5,4 @@
 0. First constant, first print
 1. 3 languages
 2. Arguments
+3. Value of my argument
