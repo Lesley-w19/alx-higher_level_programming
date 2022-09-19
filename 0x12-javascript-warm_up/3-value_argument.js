4eb0d6ac9dd690e73cea54b9e251f07da8bde95c
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-19 18:38:39 +0300

    semistandard styling

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@
  * a script that prints the first argument passed to it: argv is 2
  */
 
-if (process.argv[2] === undefined) {
+ if (process.argv[2] === undefined) {
     console.log('No argument');
   } else {
     console.log(process.argv[2]);
