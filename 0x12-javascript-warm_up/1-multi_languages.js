21cc56f67c1ed2fe3da155e9d662cabe91c3e1e6
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-19 16:53:49 +0300

    1. 3 languages Write a script that prints 3 lines: And then converted it to an executable file

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * Write a script that prints 3 lines:
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “JavaScript is amazing”
+ */
+
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
--- 0x12-javascript-warm_up/README.md
+++ 0x12-javascript-warm_up/README.md
@@ -3,3 +3,4 @@
 
 ### TASKS
 0. First constant, first print
+1. 3 languages
