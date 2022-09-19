96a68d24f43374e568acaee6c12b548b1f61d629
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-19 17:20:20 +0300

    changes to single quotes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,9 +5,9 @@
  */
 
 if (process.argv.length === 3) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else if (process.argv.length > 3) {
-  console.log("Arguments found");
+  console.log('Arguments found');
 } else {
-  console.log("No argument");
+  console.log('No argument');
 }
