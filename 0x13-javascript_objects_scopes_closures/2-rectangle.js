9b6139829e0aac923d7ab1286f938821382af34d
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-20 11:29:32 +0300

    2. Rectangle #2 -- converted 2*.js file to an executable file -- a class Rectangle that defines a rectangle:
    You must use the class notation for defining your class
    The constructor must take 2 arguments w and h
    Initialize the instance attribute width with the value of w
    Initialize the instance attribute height with the value of h
    If w or h is equal to 0 or not a positive integer, create an empty object

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+/**
+ * a class Rectangle that defines a rectangle:
+ * If w or h is equal to 0 or not a positive integer, create an empty object
+ */
+
+class Rectangle {
+    constructor (w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+}
+
+// const r1 = new Rectangle(2, 3);
+// console.log(r1);
+// console.log(r1.width);
+// console.log(r1.height);
+
+// const r2 = new Rectangle(2, -3);
+// console.log(r2);
+// console.log(r2.width);
+// console.log(r2.height);
+
+// const r3 = new Rectangle(2);
+// console.log(r3);
+// console.log(r3.width);
+// console.log(r3.height);
+
+// const r4 = new Rectangle(2, 0);
+// console.log(r4);
+// console.log(r4.width);
+// console.log(r4.height);
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/README.md
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -3,3 +3,4 @@
 ##  TASKS
 0. Rectangle #0
 1. Rectangle #1
+2. Rectangle #2
