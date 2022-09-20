909b8e2b6c8853273a4d31ff5b290fa575917f45
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-20 10:31:43 +0300

    semistandard --fix

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,8 +4,8 @@
  * Update this script by adding a new function incr that increments the integer value.
  */
 const myObject = {
-  type: "object",
-  value: 12,
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
 
