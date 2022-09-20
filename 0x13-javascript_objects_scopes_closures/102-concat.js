20db778348caafcd477133b56488fdc73cc78ee2
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-20 14:50:24 +0300

    semistandard -ffix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,16 +4,16 @@
  * Write a script that concats 2 files.
  */
 
- const fs = require('fs');
- 
- if (process.argv.length > 4) {
-   const fileA = fs.readFileSync(process.argv[2], { encoding: 'utf-8' });
-   const fileB = fs.readFileSync(process.argv[3], { encoding: 'utf-8' });
-   fs.writeFile(process.argv[4], fileA + fileB, function (errMsg) {
-     if (errMsg) {
-       return console.log(errMsg);
-     }
-   });
- } else {
-   console.log('Right format/ Usage: ./102-concat.js <filePathToSourceFile1> <filePathToSourceFile2> <filePathToDestinationFile>');
- }
+const fs = require('fs');
+
+if (process.argv.length > 4) {
+  const fileA = fs.readFileSync(process.argv[2], { encoding: 'utf-8' });
+  const fileB = fs.readFileSync(process.argv[3], { encoding: 'utf-8' });
+  fs.writeFile(process.argv[4], fileA + fileB, function (errMsg) {
+    if (errMsg) {
+      return console.log(errMsg);
+    }
+  });
+} else {
+  console.log('Right format/ Usage: ./102-concat.js <filePathToSourceFile1> <filePathToSourceFile2> <filePathToDestinationFile>');
+}
