1cde34347198fc1d16e9d4c7e891f57b95a3fadf
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-20 14:14:17 +0300

    Removed the new declared variable newNum

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -4,10 +4,9 @@
  * a function that converts a number from base 10 to another base passed as argument:
  * Prototype: exports.converter = function (base)
  */
-let newNum;
+
 exports.converter = function (base) {
   return function (num) {
-    newNum = num.toString(base);
-    return newNum;
+    return num.toString(base);
   };
 };
