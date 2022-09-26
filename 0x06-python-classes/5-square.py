f37b8834a9788b63a8fae11598359c1fdba04d4a
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-26 12:17:03 +0300

    5. Printing a square -- converted to executable fiel -- pycodestyle

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" a class Square that defines a square by: (based on 4-square.py) """
+
+
+class Square:
+    """ a class Square that defines a square by instance attribute size
+uses setters and getters to set and retrieve size int
+"""
+    def __init__(self, size=0):
+        """ define the size attribute"""
+        self.size = size
+
+    @property
+    def size(self):
+        """ retrieves and allows access to the size attribute """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Args: value: the size attribute.
+        Riases:
+        TypeError: if value is not an integer
+        ValueError: if value is not >= 0
+        """
+        self.__size = value
+        
+    def area(self):
+        """  that returns the current square area """
+        return self.__size ** 2
+    
+    def my_print(self):
+        """ that prints in stdout the square with the character # """
+        for i in range(self.__size):
+            print("#" * self.__size)
+        if (self.__size == 0):
+            print()
+        
+    
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -6,3 +6,4 @@
 2. Size validation
 3. Area of a square
 4. Access and update private attribute
+5. Printing a square
