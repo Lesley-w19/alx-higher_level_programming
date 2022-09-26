5e3ac702c628f8a2f8156963f6ab1910bc3fd2d3
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-26 12:59:22 +0300

    if not (isinstance(value[0], int) and isinstance(value[1], int)):

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,7 +46,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not (isinstance(value[0], int) and value[1], int):
+        if not (isinstance(value[0], int) and isinstance(value[1], int)):
             raise TypeError("position must be a tuple of 2 positive integers")
         if ((value[0] < 0) and (value[1] < 0)):
             raise TypeError("position must be a tuple of 2 positive integers")
