22e32cd74fd3672c4bbe817557fad6fb4b46fabf
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-26 11:20:09 +0300

    doumentation of the class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,15 @@ raise a ValueError exception with the message size must be >= 0
 
 class Square:
     def __init__(self, size=0):
-        """ conditions"""
+        """_summary_
+
+        Args:
+            size (int, optional): _description_. Defaults to 0.
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
