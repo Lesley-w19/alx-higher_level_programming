59271271cd8221900d8e567ac9cc858ba102f0a7
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-03 13:25:54 +0300

    0. Safe list printing a function that prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     num_of_list = 0
 
-    for value in range(x):
+    for value in my_list[:x]:
         try:
-            print("{}".format(my_list[value]), end='')
-        except:
-            break
-        else: 
+            print("{}".format(value), end='')
             num_of_list += 1
+        except ValueError:
+            pass
 
-        print()
-        return num_of_list
+    print()
+    return num_of_list
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -1,4 +1,6 @@
-0x05-python-exceptions
+## 0x05-python-exceptions
+
+###  TASKS
 0. Safe list printing
 1. Safe printing of an integers list
 2. Print and count integers
