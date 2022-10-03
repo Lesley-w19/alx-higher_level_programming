3eb104e16bce1fbb927605f47566146c72edb9aa
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-03 13:32:42 +0300

    pycodestyle -- changed the try statement to get  value from my_list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
 
-    for value in my_list[:x]:
+    for value in range(x):
         try:
-            print("{:d}".format(value), end='')
+            print("{:d}".format(my_list[value]), end='')
             num += 1
         except (TypeError, ValueError):
             continue
