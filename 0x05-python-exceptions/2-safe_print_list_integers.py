2a4bce12dc7bae861ce0e0d924eb57d7bd5dc510
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-03 13:26:36 +0300

    2. Print and count integers -- pycode style

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
 
-    for value in range(x):
+    for value in my_list[:x]:
         try:
-            print("{:d}".format(my_list[value]), end='')
+            print("{:d}".format(value), end='')
             num += 1
         except (TypeError, ValueError):
             continue
-        print()
-        return num
+    print()
+    return num
