6ef9ee78d696cba694d7d4fb2a90bc3f9915d228
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-06 11:43:47 +0300

    5. Raise exception -- converted to executable file --pycodestyle

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# a function that raises a type exception. """
+
+
+def raise_exception():
+    raise TypeError
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -6,3 +6,4 @@
 2. Print and count integers
 3. Integers division with debug
 4. Divide a list
+5. Raise exception
