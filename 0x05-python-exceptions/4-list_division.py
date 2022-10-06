be31b642e5d564f678954ad521146c574d3cdc54
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-06 11:33:32 +0300

    comented out the second line

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" a function that divides element by element 2 lists. """
+# a function that divides element by element 2 lists. """
 
 
 def list_division(my_list_1, my_list_2, list_length):
