e402a216898b6bac9c986d09bde29dca5baa360d
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-06 11:31:38 +0300

    4. Divide a list -- Write a function that divides element by element 2 lists.
    -- convertred to executable file
    --pycodestyle

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" a function that divides element by element 2 lists. """
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    """ function that divides element by element 2 lists. """
+    newList = list()
+
+    for l in range(list_length):
+        try:
+            newList.append(my_list_1[l] / my_list_2[l])
+        except IndexError:
+            newList.append(0)
+            print("out of range")
+        except TypeError:
+            newList.append(0)
+            print("wrong type")
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            newList.append(0)
+            print("division by 0")
+        finally:
+            pass
+    return newList
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -5,3 +5,4 @@
 1. Safe printing of an integers list
 2. Print and count integers
 3. Integers division with debug
+4. Divide a list
