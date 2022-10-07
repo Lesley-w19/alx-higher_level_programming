cdae95a8584345187c7cc52750c4a9d355c56b1f
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-07 12:14:43 +0300

    replace single with double quotes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,4 +8,4 @@ class LockedClass:
     creating new instance attributes,
     except if the new instance attribute is called first_name.
     """
-    __slots__ = ['first_name']
+    __slots__ = ["first_name"]
