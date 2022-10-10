3a0d2c59e6f041dba63a514e6f254c33fc32cf65
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-10 12:02:58 +0300

    removed the import field -- moved filedata to inside of ith statement

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,7 +2,6 @@
 """
 a function that writes an Object to a text file, using a JSON representation:
 """
-from dataclasses import field
 import json
 
 
@@ -14,7 +13,8 @@ def save_to_json_file(my_obj, filename):
         my_obj (_type_): object in JSON format
         filename (_type_): new filename to write
     """
-    file_data = json.dumps(my_obj)
+    
     with open(filename, 'w') as file:
+        file_data = json.dumps(my_obj)
         file.write(file_data)
         file.close()
