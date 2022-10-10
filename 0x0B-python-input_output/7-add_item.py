dbf3abd5080e0c861368fa0ae94c1178a67b41f8
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-10 12:59:39 +0300

    removed trailing white space

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-script that adds all arguments to a Python list,
-and then save them to a file:
-"""
+""" script that adds all arguments to a Python list,then saves"""
 
 
 import sys
 
 
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file.py')
+.load_from_json_file
 
-filename='add_item.json'
+filename = 'add_item.json'
 
 try:
     list_data = load_from_json_file(filename)
