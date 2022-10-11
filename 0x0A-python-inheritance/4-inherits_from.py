c232d6dd74c92a37397f679ca7f38be6ed51b3aa
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-11 18:17:43 +0300

    4. Only sub class of Write a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.
    Prototype: def inherits_from(obj, a_class):
    You are not allowed to import any module
    -- converted to executable file
    -- pycodestyle validation

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a function that returns True if the object is an instance of
+a class that inherited (directly or indirectly) from the
+specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """_summary_
+    issubclass -returns whetther its derived form same or other class
+    Args:
+        obj (_type_): object to be inherited
+        a_class (_type_): class to be checked
+    """
+    if type(obj) is not a_class and issubclass(type(obj), a_class):
+        return True
+    return False
+        
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -5,3 +5,4 @@
 1. My list
 2. Exact same object
 3. Same class or inherit from
+4. Only sub class of
