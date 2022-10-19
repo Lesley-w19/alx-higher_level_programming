bf17e5dcf5cddc8fd74ea7c2f17c0dbd15fe558a
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-19 20:55:26 +0300

    2. POST an email #0 ------------------ Write a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
    The email must be sent in the email variable
    You must use the packages urllib and sys
    You are not allowed to import packages other than urllib and sys
    You don’t need to check arguments passed to the script (number or type)
    You must use the with statement

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+2-post_email
+"""
+
+
+from urllib import parse, request
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    base_url = argv[1]
+    email = argv[2]
+    value = {"email": email}
+
+    data = parse.urlencode(value).encode("ascii")
+
+    with request.urlopen(base_url,data) as response:
+        content = response.read()
+
+        print("Your email is: {}".format(content))
--- 0x11-python-network_1/README.md
+++ 0x11-python-network_1/README.md
@@ -4,3 +4,4 @@
 ####### TASKS
 0. What's my status? #0
 1. Response header value #0
+2. POST an email #0
