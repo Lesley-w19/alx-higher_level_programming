afaa7134f644ee2f9e12b23ee2f90ad412e7cb1e
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-19 23:51:52 +0300

    removed white trailing space

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,13 +11,13 @@ import requests
 if __name__ == '__main__':
     repo_name = argv[1]
     repo_owner = argv[2]
-    
+
     base_url = "https://api.github.com/repos/{}/{}/commits".format(
             repo_name, repo_owner)
 
     with requests.get(base_url) as response:
         content = response.json()
-        
+
         if (len(content) == 0):
             print("No result")
         else:
