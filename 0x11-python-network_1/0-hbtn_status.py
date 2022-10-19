c5932d9beacf588293314799f72ac99ec1751e3d
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-19 17:21:50 +0300

    trailing space

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,15 +6,15 @@ from urllib import request
 
 
 if __name__ == '__main__':
-   with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    content = response.read()
-    typeContent = type(content)
-    ut8Content = content.decode('UTF-8')
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        typeContent = type(content)
+        ut8Content = content.decode('UTF-8')
 
-    print("Body response:\
+        print("Body response:\
         \n\t- type: {}\
         \n\t- content: {}\
         \n\t- utf8 content: {}\
         ".format(typeContent, content, ut8Content)
-)
+        )
    
