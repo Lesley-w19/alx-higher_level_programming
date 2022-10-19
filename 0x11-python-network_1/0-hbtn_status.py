132d287ee14b8b5df94d81da369511899614e536
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-19 17:12:40 +0300

    trailing spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,14 @@
 from urllib import request
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     content = response.read()
     ut8Content = content.decode('utf-8')
-    
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(type(content), content, ut8Content))
+
+    print("Body response:\
+        \n\t- type: {}\
+        \n\t- content: {}\
+        \n\t- utf8 content: {}\
+        ".format(type(content), content, ut8Content)
+        )
