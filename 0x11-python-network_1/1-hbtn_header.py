0f13de2440b74bc23ae4d20495f23e11c589c0af
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-19 21:10:41 +0300

    added a new method to the script

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,6 +8,11 @@ from sys import argv
 from urllib import request
 
 
+def main():
+    base_url = argv[1]
+
+    with request.urlopen(base_url) as response:
+        print(response.info().get('X-Requested-Id'))
+
 if __name__ == '__main__':
-    with request.urlopen(argv[1]) as response:
-        print(response.info().get('X-Request-Id'))
+    main()
