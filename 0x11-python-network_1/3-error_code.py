8804de7e0eab1ac6a1560572e4bbce91faa8840c
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-19 21:57:02 +0300

    3. Error code #0 ---------------------- Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).
    You have to manage urllib.error.HTTPError exceptions and print: Error code: followed by the HTTP status code
    You must use the packages urllib and sys
    You are not allowed to import other packages than urllib and sys
    You don’t need to check arguments passed to the script (number or type)
    You must use the with statement
    -------------------
    conveereted to executable file

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+3-error_code
+"""
+
+
+from sys import argv
+from urllib.error import HTTPError
+from urllib.request import urlopen
+
+
+if __name__ == '__main__':
+    base_url = argv[1]
+    try:
+        with urlopen(base_url) as response:
+            content = response.read().decode('UTF-8')
+            print(content) 
+    except HTTPError as e:
+        print("Error code: {}".format(e))
--- 0x11-python-network_1/README.md
+++ 0x11-python-network_1/README.md
@@ -5,3 +5,4 @@
 0. What's my status? #0
 1. Response header value #0
 2. POST an email #0
+3. Error code #0
