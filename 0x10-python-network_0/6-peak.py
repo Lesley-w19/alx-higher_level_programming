91d774ef3a69c2882bd260dd6f72d2ea064960e9
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-19 16:21:31 +0300

    ----- removed semicolons

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,8 +8,7 @@ def find_peak(list_of_integers):
     """
     if len(list_of_integers) <= 0:
         return None
-    
-    list_of_integers.sort();
-    
-    return list_of_integers[-1];
-    
+
+    list_of_integers.sort()
+
+    return list_of_integers[-1]
