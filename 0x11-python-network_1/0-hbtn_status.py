7ea17918803bf0e025368a0250fbb32c3bd867ee
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-19 21:29:52 +0300

    Rearranged the format specification in print

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
 
         print("Body response:\n\
 \t- type: {}\n\t- content: {}\n\t- utf8 content: {}\
-".format(type, typeContent, ut8Content))
+".format(typeContent, content, ut8Content))
