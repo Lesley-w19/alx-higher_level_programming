6ad7438941000804ad7aaa907aa5c249e9347969
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-20 15:29:29 +0300

    5-filter_cities.py:32:1: W293 blank line contains whitespace ------------------- Added the group_concat() to list all cities in one line separated by a coma

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -25,15 +25,22 @@ def main():
         db=db_name,
         charset="utf8"
     )
+
     cur = db_connect.cursor()
-    cur.execute("SELECT cty.name FROM cities cty\
-        INNER JOIN states st ON cty.state_id = st.id\
-        WHERE st.name = %s", (search_input,))
-    
+
+    cur.execute(
+    "SELECT group_concat(cty.name separator ', ')\
+    FROM cities cty\
+    INNER JOIN states st ON cty.state_id = st.id\
+    WHERE st.name = %s", (search_input,))
+
     query_rows = cur.fetchall()
 
-    for row in query_rows:
-        print(row)
+    for city in query_rows:
+        if city[0] is not None:
+            print(city[0])
+        else:
+            print()
 
     cur.close()
     db_connect.close()
