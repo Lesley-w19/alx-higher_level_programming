45c37eeb5a296b069b05d91d7cc3ae4ab073561a
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-20 13:32:52 +0300

    0-select_states.py:15:1: W293 blank line contains whitespace 0-select_states.py:16:80: E501 line too long (118 > 79 characters) 0-select_states.py:25:1: E305 expected 2 blank lines after class or function definition, found 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,8 +12,9 @@ def main():
     user = argv[1]
     passwrd = argv[2]
     db_name = argv[3]
-    
-    db_connect = MySQLdb.connect(host="localhost", port=3306, user=user, password=passwrd, db=db_name, charset="utf8")
+
+    db_connect = MySQLdb.connect(host="localhost", port=3306,
+user=user, password=passwrd, db=db_name, charset="utf8")
     cur = db_connect.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
@@ -21,6 +22,7 @@ def main():
         print(row)
     cur.close()
     db_connect.close()
+
 
 if __name__ == '__main__':
     main()
