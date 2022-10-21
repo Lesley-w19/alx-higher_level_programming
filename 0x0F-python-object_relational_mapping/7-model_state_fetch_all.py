5cb6316c86fb2716bce7c3d1feafedf1e70e28ca
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-21 10:57:53 +0300

    renamed db_name in oline 21

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,10 +15,10 @@ from sqlalchemy.orm import sessionmaker
 def main():
     username = argv[1]
     passwrd = argv[2]
-    db_name = argv[3]
+    db_n = argv[3]
 
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(username, passwrd, db_name), pool_pre_ping=True)
+        "mysql+mysqldb://{}:{}@localhost/{}".format(username, passwrd, db_n), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
