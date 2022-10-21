af91fd10915e5846f70b65bf044a8e951a77ad25
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-21 10:54:32 +0300

    removed the white trailing spaces at start of sentences

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,14 +16,14 @@ def main():
     username = argv[1]
     passwrd = argv[2]
     db_name = argv[3]
-    
+
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}".format(username, passwrd, db_name), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
-    
+
     Session = sessionmaker(bind=engine)
-    
+
     session = Session()
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
