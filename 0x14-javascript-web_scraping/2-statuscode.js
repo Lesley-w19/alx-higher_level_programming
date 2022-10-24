b402096d01bd23d2486ccaab0efa25230e8a4367
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-24 15:26:06 +0300

    semistandard --fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,16 +2,16 @@
 const request = require('request');
 
 try {
-    const url = process.argv[2];
-    request(
-        url,
-        (err, response, body) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log("code:", response && response.statusCode);
-        }
-    );
+  const url = process.argv[2];
+  request(
+    url,
+    (err, response, body) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log('code:', response && response.statusCode);
+    }
+  );
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
