704cd837fd94b142485df48063ab08e275d49676
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-24 14:12:35 +0300

    removed indentation

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,15 +2,15 @@
 const fs = require('fs');
 
 try {
-    const file = process.argv[2];
-    const data = fs.readFileSync(
-        file,
-        {
-            encoding: 'utf-8',
-            flag:'r'
-        }
-    );
-    console.log(data);
+  const file = process.argv[2];
+  const data = fs.readFileSync(
+    file,
+    {
+      encoding : 'utf-8',
+      flag :'r'
+    }
+  );
+  console.log(data);
 } catch (error) {
     console.log(error);
 }
