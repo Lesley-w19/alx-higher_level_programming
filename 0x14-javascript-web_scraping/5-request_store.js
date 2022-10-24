2ad20a89ec21766ea226131cc5fa481ff75b480d
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-24 16:38:41 +0300

    semistandard validation

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,32 +1,31 @@
 #!/usr/bin/node
-const req = require('/usr/lib/node_modules/request');
+const req = require('request');
 const fs = require('fs');
 
 try {
-    const url = process.argv[2];
-    const file = process.argv[3];
+  const url = process.argv[2];
+  const file = process.argv[3];
 
-    req(
-        url,
+  req(
+    url,
+    {
+      json: true
+    },
+    (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      fs.writeFileSync(
+        file,
+        body,
         {
-            json: true
-        },
-        (error, response, body) =>{
-            if (error){
-                console.log(error);
-            }
-        fs.writeFileSync(
-            file,
-            body,
-            {
-                encoding: 'utf8',
-                flag: 'w'
-            }
-        )
+          encoding: 'utf8',
+          flag: 'w'
         }
-        
-    )
-    
+      );
+    }
+
+  );
 } catch (error) {
-    console.log(error)
+  console.log(error);
 }
