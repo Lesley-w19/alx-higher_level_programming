8a77932019252add41d3384eb812587db4f96938
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-25 11:44:24 +0300

    semistandard validation fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,37 +1,36 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 
 try {
-    const movieId = process.argv[2];
+  const movieId = process.argv[2];
 
-    request(
+  request(
         `https://swapi-api.hbtn.io/api/films/${movieId}`,
         {
-            json: true,
+          json: true
         },
         (error, response, body) => {
-             if (error){
-                console.log(error);
-             }
-            const characters = body.characters;
+          if (error) {
+            console.log(error);
+          }
+          const characters = body.characters;
 
-            characters.forEach(element => {
-                request(
-                    element,
-                    {
-                        json: true
-                    },
-                    (error, response, body) => {
-                        if (error){
-                            console.log(error)
-                        }
-                        console.log(body.name)
-                    }
-                )
-             });
+          characters.forEach(element => {
+            request(
+              element,
+              {
+                json: true
+              },
+              (error, response, body) => {
+                if (error) {
+                  console.log(error);
+                }
+                console.log(body.name);
+              }
+            );
+          });
         }
-    )
-    
+  );
 } catch (error) {
-    console.log(error)
+  console.log(error);
 }
