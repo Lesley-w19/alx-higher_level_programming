70ad67237df4659d55b5eee3489fd47feadb0df6
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-25 11:57:39 +0300

    a script that prints all characters of a Star Wars movie:

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require('/usr/lib/node_modules/request');
 
 try {
   const movieId = process.argv[2];
@@ -9,26 +9,32 @@ try {
         {
           json: true
         },
-        (error, response, body) => {
+        async (error, response, body) => {
           if (error) {
             console.log(error);
           }
           const characters = body.characters;
 
-          characters.forEach(element => {
-            request(
-              element,
-              {
-                json: true
-              },
-              (error, response, body) => {
-                if (error) {
-                  console.log(error);
+          for (const element of characters) {
+            const name = await new Promise((resolve, reject) => {
+              request(
+                element,
+                {
+                  json: true
+                },
+                (error, response, body) => {
+                  if (error) {
+                    return reject(error);
+                  }
+
+                  resolve(body.name);
                 }
-                console.log(body.name);
-              }
-            );
-          });
+              );
+            });
+            if (name !== undefined) {
+              console.log(name);
+            }
+          }
         }
   );
 } catch (error) {
