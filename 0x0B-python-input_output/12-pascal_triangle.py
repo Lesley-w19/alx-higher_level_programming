06645d4dd10621f53cb3d7e38cc50f862ac11699
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-31 21:28:02 +0300

    new line added at the end of the file

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -17,10 +17,10 @@ def pascal_triangle(n):
     if n <= 0:
         return triangle
 
-    
+    triangle = [[0 for x in range(i + 1)] for i in range(n)]
     triangle[0] = [1]
 
-    for i in range(1, n - 1):
+    for i in range(1, n):
         triangle[i][0] = 1
         for j in range(1, i + 1):
             if j < len(triangle[i - 1]):
