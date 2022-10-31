69ec45c64c3949902024fe37cda3864b65bc238f
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-31 19:49:55 +0300

    converted to double quotes ------ for loop paameter loops data

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,17 +5,18 @@
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
 
-filename = 'add_item.json'
+filename = "add_item.json"
 
 try:
     list_data = load_from_json_file(filename)
 except Exception as e:
     list_data = []
 
-for arg in sys.argv[1:]:
-    list_data.append(arg)
+for data in sys.argv[1:]:
+    list_data.append(data)
+    print(list_data)
 
 save_to_json_file(list_data, filename)
